11beb87b1019a9923f7af411aa30215a48402311
Rian <febrian.bahariadi@gmail.com>
2024-06-28 11:16:12 +0700

    tampilkan Metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ def main() :
     st.latex('c^2 = a^2+b^2')
     st.write('Contoh dataframe')
     st.dataframe(df_house_clean)
-
+    st.write('Metrics')
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 if __name__ == '__main__' : 
     main()
