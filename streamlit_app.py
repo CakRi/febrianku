6fed5a11ce004b021acb959737e6bf644fc8db34
Rian <febrian.bahariadi@gmail.com>
2024-06-28 13:49:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,18 +45,18 @@ def main() :
     col1, col2, col3 = st.columns(3)
 
     with col1:
-       st.header("A cat")
-       st.image("https://static.streamlit.io/examples/cat.jpg")
-    # atau dengan assignment 
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+        # atau dengan assignment 
     image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
 
     with col2:
-       st.header("A dog")
-       st.image("https://static.streamlit.io/examples/dog.jpg")
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
 
     with col3:
-       st.header("An owl")
-       st.image("https://static.streamlit.io/examples/owl.jpg")
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
         
 if __name__ == '__main__' : 
     main()
